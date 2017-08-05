7aacb527e78ca4d042e715c19b08ec4032c7dd34
taotaoFighting <taotaoFighting2016@163.com>
2017-08-05 11:02:06 +0800

    新增 菜系枚举接口

--- llz/src/main/java/com/pm/controller/EnumController.java
+++ llz/src/main/java/com/pm/controller/EnumController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pm.entity.EnumCuisine;
@@ -16,7 +17,7 @@ public class EnumController {
 	@Autowired
 	EnumCuisineService enumCuisineService;
 
-	@RequestMapping(value="/cuisine/list")
+	@RequestMapping(value="/cuisine/list",method=RequestMethod.GET)
 	public List<EnumCuisine> cuisineList() {
 		
 		return enumCuisineService.cuisineList();
